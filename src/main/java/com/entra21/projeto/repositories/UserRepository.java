87b7ffb6513709083919f9486e86298c27e40291
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-09-05 20:46:21 -0300

    Criado findByName na Interface UserRepository

--- src/main/java/com/entra21/projeto/repositories/UserRepository.java
+++ src/main/java/com/entra21/projeto/repositories/UserRepository.java
@@ -8,11 +8,14 @@ import com.entra21.projeto.model.Users;
 /**
  * Repositório de Usuários
  * Criado em 11/08/2022
+ * Atualizado em 05/09/2022
  * 
  * @author Ramon Casagrande
  */
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    String findByEmail(String email);
     
 }
