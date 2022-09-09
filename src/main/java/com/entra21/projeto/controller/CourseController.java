8885ba62925454a9328fc1729061545baa72e968
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-09-08 22:56:41 -0300

    Criado findById no CourseController

--- src/main/java/com/entra21/projeto/controller/CourseController.java
+++ src/main/java/com/entra21/projeto/controller/CourseController.java
@@ -1,8 +1,12 @@
 package com.entra21.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +20,14 @@ public class CourseController {
     @Autowired
     private CourseRepository courseRepository;
     
+    @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Optional<Course>> setCourse(@PathVariable Long id){
+        Optional<Course> courseOpt = courseRepository.findById(id);
+        return ResponseEntity.ok(courseOpt);
     }
 }
